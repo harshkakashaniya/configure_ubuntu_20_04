82d0192b37de159e352e9f52ff7a053011df8fb9
Harsh Kakashaniya <harsh.kakashniya@gmail.com>
2023-09-17 15:19:19 -0500

    Create install.bash for chrome , docker , ros noetic

---
+++ install.bash
@@ -0,0 +1,16 @@
+# install Chrome
+sudo apt update
+sudo apt install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb 
+
+#install docker
+sudo snap install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+
+#install ros
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop-full
