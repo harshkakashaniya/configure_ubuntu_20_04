e9d75c42393c54bb77b7e0b0618168fd8187b2ce
Harsh Kakashaniya <harsh.kakashniya@gmail.com>
2024-03-31 17:14:54 -0500

    Update install.bash

--- install.bash
+++ install.bash
@@ -13,5 +13,5 @@ sudo sh ./get-docker.sh
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop-full
+sudo apt install -y ros-noetic-desktop-full
 
